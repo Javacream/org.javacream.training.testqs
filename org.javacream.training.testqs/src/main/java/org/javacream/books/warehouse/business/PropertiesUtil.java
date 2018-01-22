f760b9ef86c4dd42c1a5f69a6b86adccfab585bd
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-01-22 07:32:12 +0100

    implement simple project

---
+++ org.javacream.training.testqs/src/main/java/org/javacream/books/warehouse/business/PropertiesUtil.java
@@ -0,0 +1,19 @@
+package org.javacream.books.warehouse.business;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public abstract class PropertiesUtil {
+
+	public static Properties getProperties(String name) {
+		Properties properties = new Properties();
+		try {
+			properties.load(PropertiesUtil.class.getResourceAsStream(name));
+			return properties;
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException(e.getMessage());
+		}
+
+	}
+}
---
+++ org.javacream.training.testqs/src/main/java/org/javacream/books/warehouse/business/SimpleStoreService.java
@@ -0,0 +1,23 @@
+package org.javacream.books.warehouse.business;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+public class SimpleStoreService {
+
+	private HashMap<String, Properties> store;
+
+	{
+		store = new HashMap<String, Properties>();
+		store.put("books", PropertiesUtil.getProperties("books-store.properties"));
+	}
+
+	public int getStock(String category, String id) {
+		try {
+			return Integer.parseInt(store.get(category).get(id).toString());
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+
+}
