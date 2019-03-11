f0467c0298bd50f3638c99465e21598bd7b3b57c
Rainer Sawitzki <training@rainer-sawitzki>
2019-03-11 14:09:13 +0100

    add start for MathUtilExample add SpecifiedBy
    MathUtil.java
    SpecifiedBy.java

---
+++ org.javacream.training.testqs/src/main/java/org/javacream/util/MathUtil.java
@@ -0,0 +1,17 @@
+package org.javacream.util;
+
+import org.javacream.util.testqs.SpecifiedBy;
+
+public class MathUtil {
+	
+	@SpecifiedBy(document="Flipchart...", version="1.0")
+	public double areaOfRectangle(double height, double width) {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	@SpecifiedBy(document="Flipchart...", version="1.0")
+	public double perimeterOfRectangle(double height, double width) {
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+}
